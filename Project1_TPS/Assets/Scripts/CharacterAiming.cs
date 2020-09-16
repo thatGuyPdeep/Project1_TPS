18f1e422d6eff35a1a7e6b1521de14088758ffd7
8bit <47435283+thatGuyPdeep@users.noreply.github.com>
2020-09-16 18:00:31 +0530

    Added features
    character movement
    character aiming
    shooting mechanics
    particle effects
    bullet trails
    bullet drop

---
+++ Project1_TPS/Assets/Scripts/CharacterAiming.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations.Rigging;
+
+public class CharacterAiming : MonoBehaviour
+{
+    public float turnSpeed = 15;
+    public float aimDuration = 0.3f;
+    public Rig aimLayer;
+
+    Camera mainCamera;
+    RaycastWeapon weapon;
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        weapon = GetComponentInChildren<RaycastWeapon>();
+    }
+
+
+    void FixedUpdate()
+    {
+        float yawCamera = mainCamera.transform.rotation.eulerAngles.y;
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, yawCamera, 0), turnSpeed * Time.fixedDeltaTime);
+    }
+
+    private void LateUpdate(){
+        if(aimLayer){
+            if(Input.GetButton("Fire2")) {
+                aimLayer.weight += Time.deltaTime / aimDuration;
+            } else {
+                aimLayer.weight -= Time.deltaTime / aimDuration;
+            }
+        }
+
+        if(Input.GetButtonDown("Fire1")){
+            weapon.StartFiring();
+        } 
+        if(weapon.isFiring){
+            weapon.UpdateFiring(Time.deltaTime);
+        }
+        weapon.UpdateBullets(Time.deltaTime);
+        if(Input.GetButtonUp("Fire1")){
+            weapon.StopFiring();
+        }
+        
+    }
+}
---
+++ Project1_TPS/Assets/Scripts/RaycastWeapon.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastWeapon : MonoBehaviour
+{
+    class Bullet{
+        public float time;
+        public Vector3 initialPosition;
+        public Vector3 initialVelocity;
+        public TrailRenderer tracer;
+    }
+
+    public bool isFiring = false;
+    public int fireRate = 25;
+    public float bulletSpeed = 1000f;
+    public float bulletDrop = 0.0f;
+    public ParticleSystem[] muzzleFlash;
+    public ParticleSystem hitEffect;
+    public TrailRenderer tracerEffect;
+
+    public Transform raycastOrigin;
+    public Transform raycastDestination;
+
+    Ray ray;
+    RaycastHit hitInfo;
+    float accumulatedTime;
+    List<Bullet> bullets = new List<Bullet>();
+    float maxLifeTime = 3.0f;
+
+    Vector3 GetPosition(Bullet bullet){
+        // p + v*t + 0.5*g*t*t
+        Vector3 gravity = Vector3.down * bulletDrop;
+        return(bullet.initialPosition) + (bullet.initialVelocity * bullet.time) + (0.5f * gravity * bullet.time * bullet.time);
+    }
+
+    Bullet CreateBullet(Vector3 position, Vector3 velocity){
+        Bullet bullet = new Bullet();
+        bullet.initialPosition = position;
+        bullet.initialVelocity = velocity;
+        bullet.time = 0.0f;
+        bullet.tracer = Instantiate(tracerEffect, position, Quaternion.identity);
+        bullet.tracer.AddPosition(position);
+        return bullet;
+    }
+
+    public void StartFiring(){
+        isFiring = true;
+        accumulatedTime = 0.0f;
+        FireBullet();
+    }
+
+    public void UpdateFiring(float deltaTime){
+        accumulatedTime += deltaTime;
+        float fireInterval = 1.0f / fireRate;
+        while(accumulatedTime >= 0.0f){
+            FireBullet();
+            accumulatedTime -= fireInterval;
+        }
+    }
+
+    public void UpdateBullets(float deltaTime){
+        SimulateBullets(deltaTime);
+        DestroyBullets();
+    }
+
+    void SimulateBullets(float deltaTime){
+        bullets.ForEach(bullet => {
+            Vector3 p0 = GetPosition(bullet);
+            bullet.time += deltaTime;
+            Vector3 p1 = GetPosition(bullet);
+            RaycastSegment(p0, p1, bullet);
+        });
+    }
+
+    void DestroyBullets() {
+        bullets.RemoveAll(bullet => bullet.time > maxLifeTime);
+    }
+
+    void RaycastSegment(Vector3 start, Vector3 end, Bullet bullet){
+        Vector3 direction = end - start;
+        float distance = (end - start).magnitude;
+        ray.origin = start;
+        ray.direction = direction;
+        if(Physics.Raycast(ray, out hitInfo, distance)){
+            hitEffect.transform.position = hitInfo.point;
+            hitEffect.transform.forward = hitInfo.normal;
+            hitEffect.Emit(1);
+
+            bullet.tracer.transform.position = hitInfo.point;
+            bullet.time = maxLifeTime;
+        } else{
+             bullet.tracer.transform.position = end;
+        }
+    }
+
+    public void FireBullet(){
+        foreach(var particle in muzzleFlash){
+            particle.Emit(1);
+        }
+        
+        Vector3 velocity = (raycastDestination.position - raycastOrigin.position).normalized * bulletSpeed;
+        var bullet = CreateBullet(raycastOrigin.position, velocity);
+        bullets.Add(bullet);
+    }
+
+    public void StopFiring(){
+        isFiring = false;
+    }
+}
